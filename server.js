7710685c7a35b4096cb53120bb56994d7efac624
Sidney Henderson <shende39@uncc.edu>
2020-09-29 18:26:47 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,24 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('./myjson.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        const myBudget = JSON.parse(data);
+        res.json(myBudget);
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
