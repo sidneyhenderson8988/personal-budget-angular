3b3767721b83d7da2b867377f378264c58b76a2d
Sidney Henderson <shende39@uncc.edu>
2020-10-07 10:32:09 -0400

    Fixed chart for-loop issue

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -38,12 +38,12 @@ labels: []
       for (var i = 0; i < res.myBudget.length; i++){
         this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
         this.dataSource.labels[i] = res.myBudget[i].title;
-        this.createChart();
+
       }
+      this.createChart();
     });
-
-
   }
+
   // tslint:disable-next-line: typedef
   createChart() {
     // tslint:disable: no-var-keyword
